6dfaebfe56d98672f19643a2c92cfc913caacaf5
dianjixz <18637716021@163.com>
2023-04-03 14:31:55 +0800

    [update]

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 # CM4Stack_LvglDemo
 
-Lvgl simple clock demo for CM4STACK
+Lvgl Demo for CM4STACK
 
-![](https://github.com/m5stack/m5stack-linux-dtoverlays/blob/main/examples/Lvgl/SimpleClock/pic.png?raw=true)
+![](https://github.com/m5stack/m5stack-linux-dtoverlays/blob/main/examples/Lvgl/Basic/pic.png?raw=true)
 
 #### File tree
 
 ```
 .
 ├── CMakeLists.txt
-├── CopyLib.sh
 ├── README.md
-├── UI
 ├── lv_conf.h
 ├── lv_drv_conf.h
 ├── lv_porting
@@ -36,26 +34,15 @@ sudo apt update
 sudo apt install libsdl2-dev
 ```
 
-#### Copy the Lvgl related library from example/Basic
-
-```shell
-# Run copy script
-./CopyLib.sh
-
-# Or Manually
-cp -r ../Basic/lv_porting .
-cp -r ../Basic/lvgl .
-```
-
 #### Build
 
-```shell
+```
 mkdir build && cd build
 cmake .. && make
 ```
 
 #### Run
 
-```shell
+```
 ./cm4LvglDemo
 ```
--- main.cpp
+++ main.cpp
@@ -24,14 +24,6 @@
 #include "lv_porting/lv_port_disp.h"
 #include "lv_porting/lv_port_indev.h"
 
-#include "ui.h"
-#include "time.h"
-#include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-
-static void data_update(lv_timer_t * timer);
-
 
 int main(int argc, char const *argv[])
 {
@@ -50,11 +42,11 @@ int main(int argc, char const *argv[])
     #endif
     
 
-    ui_init();
-    lv_timer_t * timer = lv_timer_create(data_update, 1000, NULL);
-    data_update(NULL);
-    GestureDown(NULL);
-    cover_pull_up_Animation(ui_CoverPanel, 2000);
+    /* Lvgl offical demos */
+    lv_demo_widgets();
+    // lv_demo_stress();
+    // lv_demo_benchmark();
+    // lv_demo_music();
 
 
     while(1)
@@ -65,93 +57,3 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-
-char* get_IP_address()
-{
-    int fd;
-    struct ifreq ifr;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    /* I want to get an IPv4 IP address */
-    ifr.ifr_addr.sa_family = AF_INET;
-    /* I want IP address attached to "eth0" */
-    strncpy(ifr.ifr_name, "wlan0", IFNAMSIZ-1);
-    ioctl(fd, SIOCGIFADDR, &ifr);
-    close(fd);
-    /* display result */
-    static char ip_address[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr), ip_address, INET_ADDRSTRLEN);
-    // printf("%s\n", ip_address);
-    return ip_address;
-}
-
-
-char* get_host_name()
-{
-    static char hostname[1024];
-    gethostname(hostname, sizeof(hostname));
-    return hostname;
-}
-
-
-double get_cpu_load()
-{
-    double load_avg[3];
-    getloadavg(load_avg, 3);
-    return load_avg[0];
-}
-
-
-double get_memory_usage()
-{
-    FILE* file = fopen("/proc/meminfo", "r");
-    unsigned long mem_total, mem_free;
-    fscanf(file, "MemTotal: %lu kB\nMemFree: %lu kB", &mem_total, &mem_free);
-    fclose(file);
-
-    double mem_usage = (double)(mem_total - mem_free) / mem_total * 100;
-    // printf("%ld %ld %f\n", mem_total, mem_free, mem_usage);
-    return mem_usage;
-}
-
-
-void data_update(lv_timer_t * timer)
-{
-    /* get time */
-    time_t result = time(NULL);   
-    tm* time_now = localtime(&result);
-    lv_label_set_text_fmt(ui_TimeHour, "%.2d", time_now->tm_hour);
-    lv_label_set_text_fmt(ui_TimeMinute, "%.2d", time_now->tm_min);
-
-    /* update sys infos */
-    char data_buff[100];
-    snprintf(data_buff, sizeof(data_buff), "> Host: %.12s\n> IP: %s\n> Load: %.2f\n> Mem: %.2f%%",
-                get_host_name(),
-                get_IP_address(),
-                get_cpu_load(),
-                get_memory_usage());
-    lv_label_set_text(ui_LabelSysInfos, data_buff);
-}
-
-
-void GestureUp(lv_event_t * e)
-{  
-    // printf("up %d\n", lv_obj_get_y_aligned(ui_CoverPanel));
-	if (lv_obj_get_y_aligned(ui_CoverPanel) == 0) {
-        cover_pull_up_Animation(ui_CoverPanel, 0);
-    }
-}
-
-
-void GestureDown(lv_event_t * e)
-{
-    // printf("down %d\n", lv_obj_get_y_aligned(ui_CoverPanel));
-	if (lv_obj_get_y_aligned(ui_CoverPanel) == -134) {
-        cover_pull_down_Animation(ui_CoverPanel, 0);
-        lv_obj_set_y(ui_LogoC, -190);
-        lv_obj_set_y(ui_LogoM, -190);
-        lv_obj_set_y(ui_Logo4, -190);
-        logo_popout_Animation(ui_LogoC, 300 + 0);
-        logo_popout_Animation(ui_LogoM, 300 + 100);
-        logo_popout_Animation(ui_Logo4, 300 + 200);
-    }
-}
